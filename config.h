a596dadc6acf18d17abd02e60f5cc7962017819d
jhanger-cb <jhanger@cloudbees.com>
2023-10-31 08:09:55 -0700

    Created FEATURE Branch

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 #define INCLUDE_GUARD
 
 #define PROJECT_NAME "jlp"
-#define PROJECT_VER  "0.3.0"
+#define PROJECT_VER  "0.3.1"
 #define PROJECT_VER_MAJOR "0"
 #define PROJECT_VER_MINOR "3"
-#define PTOJECT_VER_PATCH "0"
+#define PTOJECT_VER_PATCH "1"
 
 #endif // INCLUDE_GUARD
