618dad58c83d8ad5ab44c4659533d436f9614edc
jhanger-cb <jhanger@cloudbees.com>
2023-10-31 08:02:16 -0700

    updated version with cmake as didn't do that last commit, adding to release, ex post facto

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 #define INCLUDE_GUARD
 
 #define PROJECT_NAME "jlp"
-#define PROJECT_VER  "0.2.1"
+#define PROJECT_VER  "0.3.0"
 #define PROJECT_VER_MAJOR "0"
-#define PROJECT_VER_MINOR "2"
-#define PTOJECT_VER_PATCH "1"
+#define PROJECT_VER_MINOR "3"
+#define PTOJECT_VER_PATCH "0"
 
 #endif // INCLUDE_GUARD
